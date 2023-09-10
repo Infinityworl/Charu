e184d2fc53b24cb9d2b714d5dc7d6269a60994c4
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2023-09-10 15:27:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+from pyrogram import Clinet, filters
+
+
+API_ID = "24008761"
+API_HASH = "c6bb29db832216220e1234b163233cec"
+BOT_TOKEN = "6692368460:AAEAEnhNL6LAtupuF6-ko4UugdL78y9rI_I"
+
+Infinity = Client(
+    name="charu",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+
+@infinity.on_message(filters.command("start"))
+async def start_cmd(Client, message):
+    print("START Command")
+
+
+@infinity.on_message(filters.command("help"))
+async def help_cmd(Client, massage):
+    print("HELP Command")
+            
+
+print("bot was started")
+
+Infinity.run()
+
+  
 
