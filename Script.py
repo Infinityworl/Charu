98fd331229e21d2bbdb718aab65c8740f591c1cb
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2023-09-12 13:03:05 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -231,15 +231,3 @@ Wᴇʟᴄᴏᴍᴇ ᴛᴏ {chat}.
  
 
 
-    
-🏷 Title</b>: <a href={url}>{title}</a>
-🎭 Genres: {genres}
-📆 Year: <a href={url}/releaseinfo>{year}</a>
-🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
-☀️ Languages : <code>{languages}</code>
-📀 RunTime: {runtime} Minutes
-📆 Release Info : {release_date}
-🎛 Countries : <code>{countries}</code>
-⏰ Result Shown in: {remaining_seconds} 
-
-💁‍♂️ 𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐁𝐲 : {message.from_user.mention}
