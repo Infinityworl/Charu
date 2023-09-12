af85fcfa0c8ba2a7c16e67b2b0457a55839f7047
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2023-09-12 12:18:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -36,7 +36,7 @@ async def start(client, message):
             InlineKeyboardButton("➕️ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀᴛ ➕", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton("Sᴇᴀʀᴄʜ ☘️", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("Cʜᴀɴɴᴇʟ 🎬", url="https:/InlineKeyboardButton("Cʜ/t.me/infinity_Lk")
+            InlineKeyboardButton("Cʜᴀɴɴᴇʟ 🎬", url="https://t.me/infinity_Lk")
             ],[ 
             InlineKeyboardButton("CONTACT 🌐", url="https://t.me/about_nimsara")
             ],[
