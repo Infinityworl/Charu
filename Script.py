52d66245543eace2584c5e5c814879fbe026a2ed
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2023-09-12 13:01:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -221,6 +221,16 @@ Wᴇʟᴄᴏᴍᴇ ᴛᴏ {chat}.
 ꜱʜᴀʀᴇ & ꜱᴜᴩᴩᴏʀᴛ, ʀᴇqᴜᴇꜱᴛ ʏᴏᴜ ᴡᴀɴᴛᴇᴅ ᴍᴏᴠɪᴇꜱ"""
   
     IMDB_TEMPLATE = """<b>Qᴜᴇʀʏ: {query}</b>
+
+🏷 Tɪᴛʟᴇ: <a href={url}>{title}</a>
+🎭 Gᴇɴʀᴇꜱ: {genres}
+📆 Yᴇᴀʀ: <a href={url}/releaseinfo>{year}</a>
+🌟 Rᴀᴛɪɴɢ: <a href={url}/ratings>{rating}</a>/10"""
+   
+  
+ 
+
+
     
 🏷 Title</b>: <a href={url}>{title}</a>
 🎭 Genres: {genres}
